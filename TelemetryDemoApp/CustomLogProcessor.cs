84bebaf197a430b320041759defe906e9d9cdc35
Rubenso <Rubens.tueux@cy-tech.fr>
2024-07-17 17:33:19 +0200

    ajout fichiers de config

---
+++ TelemetryDemoApp/CustomLogProcessor.cs
@@ -0,0 +1,42 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+
+public class CustomLogProcessor : BaseProcessor<LogRecord>
+{
+    public override void OnEnd(LogRecord data)
+    {
+        // Custom state information
+        var logState = new List<KeyValuePair<string, object?>>
+        {
+            new("ProcessID", Environment.ProcessId),
+            new("DotnetFramework", RuntimeInformation.FrameworkDescription),
+            new("Runtime", RuntimeInformation.RuntimeIdentifier),
+            new("test", data.TraceId)
+        };
+        
+        // Example of masking sensitive data
+        if (data.Attributes != null)
+        {
+            var attributes = data.Attributes.ToList();
+            
+            // Find a key value pair with key "password" and update its value to "masked value"
+            var foundPair = attributes.Find(kvp => kvp.Key.Equals("password", StringComparison.OrdinalIgnoreCase));
+            if (!foundPair.Equals(default(KeyValuePair<string, object?>)))
+            {
+                // Find the index of the original pair in the list
+                var index = attributes.IndexOf(foundPair);
+
+                // Replace the original pair with the updated pair at the same index
+                attributes[index] = new KeyValuePair<string, object?>(foundPair.Key, "masked value");
+                data.FormattedMessage = "Message masked due to sensitive data";
+            }
+
+            data.Attributes = new ReadOnlyCollectionBuilder<KeyValuePair<string, object?>>(attributes.Concat(logState))
+                .ToReadOnlyCollection();
+        }
+
+        base.OnEnd(data);
+    }
+}
--- TelemetryDemoApp/Program.cs
+++ TelemetryDemoApp/Program.cs
@@ -1,6 +1,59 @@
+using System.Runtime.InteropServices;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Security;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenTelemetryTracing(tracerProviderBuilder =>
+{
+    tracerProviderBuilder
+        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("MyAspNetCoreService"))
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddOtlpExporter(options =>
+        {
+            options.Endpoint = new Uri("http://localhost:4317");
+        });
+});
+
+var resourceBuilder = ResourceBuilder.CreateDefault()
+    .AddService(serviceName: "Cosoluce.RandomApp.LogsDemoApi", serviceVersion: "1.0.1")
+    .AddTelemetrySdk()
+    .AddAttributes(new Dictionary<string, object>
+    {
+        ["Nom/ID du host"] = Environment.MachineName,
+        ["Système d'exploitation"] = RuntimeInformation.OSDescription,
+        ["environnement"] =
+            builder.Environment.EnvironmentName.ToLowerInvariant(),
+        ["texte aléatoire"] = "Je rajoute ce que je veux"
+        
+    });
+
+builder.Logging.ClearProviders()
+    .AddOpenTelemetry(loggerOptions =>
+    {
+        loggerOptions
+            .SetResourceBuilder(resourceBuilder)
+            .AddProcessor(new CustomLogProcessor())
+            .AddConsoleExporter();
+
+        loggerOptions.IncludeFormattedMessage = true;
+        loggerOptions.IncludeScopes = true;
+        loggerOptions.ParseStateValues = true;
+    });
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (ILogger<Program> logger) =>
+{
+    logger.LogCritical("Alerte");
+    return("Salut");
+}); 
 
 app.Run();
